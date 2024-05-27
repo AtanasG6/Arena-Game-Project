69968e31d0b2443bd35d56562dd03a39a5b7c554
atanas.ig <gyulchev666@gmail.com>
2024-05-27 20:38:04 +0300

    Add Repository class and IRepository interface

---
+++ ArenaGame/ArenaGame/Repositories/Contracts/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ArenaGame.Repositories.Contracts
+{
+	public interface IRepository<T>
+	{
+		void Add(T item);
+		bool Remove(T item);
+		T FindByName(string name);
+		IReadOnlyCollection<T> GetAll();
+	}
+}
---
+++ ArenaGame/ArenaGame/Repositories/Repository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using ArenaGame.Repositories.Contracts;
+
+namespace ArenaGame.Repositories
+{
+	public class Repository<T> : IRepository<T>
+	{
+		private readonly List<T> items;
+
+		public Repository()
+		{
+			items = new List<T>();
+		}
+
+		public void Add(T item)
+		{
+			items.Add(item);
+		}
+
+		public bool Remove(T item)
+		{
+			return items.Remove(item);
+		}
+
+		public T FindByName(string name)
+		{
+			return items.Find(item => (item as IHero)?.Name == name);
+		}
+
+		public IReadOnlyCollection<T> GetAll()
+		{
+			return items.AsReadOnly();
+		}
+	}
+}
