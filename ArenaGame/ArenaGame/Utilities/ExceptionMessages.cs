94f258f3e8b31d64e50e7e77db6ea7bdd46bc4eb
atanas.ig <gyulchev666@gmail.com>
2024-05-27 20:31:54 +0300

    Add ExceptionMessages class for standardized exception messages

---
+++ ArenaGame/ArenaGame/Utilities/ExceptionMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace ArenaGame.Utilities
+{
+	public static class ExceptionMessages
+	{
+		public const string InvalidHeroType = "Invalid hero type!";
+		public const string InvalidWeaponType = "Invalid weapon type!";
+		public const string InvalidHeroName = "Invalid hero name(s) provided.";
+		public const string InvalidPetType = "Invalid pet type provided.";
+	}
+}
