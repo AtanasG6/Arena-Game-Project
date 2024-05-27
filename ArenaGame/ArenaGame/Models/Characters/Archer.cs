352e646e88d195421e43256063d32cc0454ccc87
atanas.ig <gyulchev666@gmail.com>
2024-05-27 20:30:09 +0300

    Add all hero classes: Knight, Assassin, Archer, Mage with necessary implementations

---
+++ ArenaGame/ArenaGame/Models/Characters/Archer.cs
@@ -0,0 +1,23 @@
+﻿using ArenaGame.Controllers;
+using ArenaGame.IO.Contracts;
+using ArenaGame.Models.Contracts;
+using ArenaGame.Utilities;
+using ArenaGame.Weapons;
+
+namespace ArenaGame.Heroes
+{
+	public class Archer : Hero
+	{
+		public Archer(string name, double armor, double strength, IWeapon weapon, IPet pet, IWriter writer, ShopController shopController)
+					: base(name, armor, strength, weapon, pet, writer, shopController)
+		{
+		}
+
+		public override void LevelUp()
+		{
+			Level++;
+			Health += GameConstants.HealthBoostPerLevel;
+			Strength += GameConstants.AttackBoostPerLevel + 2;
+		}
+	}
+}
---
+++ ArenaGame/ArenaGame/Models/Characters/Knight.cs
@@ -0,0 +1,23 @@
+﻿using ArenaGame.Controllers;
+using ArenaGame.IO.Contracts;
+using ArenaGame.Models.Contracts;
+using ArenaGame.Utilities;
+using ArenaGame.Weapons;
+
+namespace ArenaGame.Heroes
+{
+	public class Knight : Hero
+	{
+		public Knight(string name, double armor, double strength, IWeapon weapon, IPet pet, IWriter writer, ShopController shopController)
+			: base(name, armor, strength, weapon, pet, writer, shopController)
+		{
+		}
+
+		public override void LevelUp()
+		{
+			Level++;
+			Health += GameConstants.HealthBoostPerLevel + 5;
+			Strength += GameConstants.AttackBoostPerLevel + 2;
+		}
+	}
+}
